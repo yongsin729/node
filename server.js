849b79b6c5c873b256271a9925254aa5cc40b3bd
yunghsin Lee <lenalee7963@gmail.com>
2022-01-19 11:35:09 +0900

    new commit4

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ app.use(express.urlencoded({extended: true})) ;
 
 const MongoClient = require('mongodb').MongoClient;
 app.set('view engine','ejs');
+
+app.use('/public',express.static('public'));//스태틱 파일을 보관하기 위해public 폴더를 사용. 
 MongoClient.connect('mongodb+srv://yongsin729:7963dt@cluster0.rscxp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 function(에러,client){
     //연결되면 할 일
@@ -26,16 +28,28 @@ app.get('/pet',function(요청,응답){
     응답.send('반갑습니다');
 });
 
+
+
 app.get('/',function(요청,응답){
     응답.sendFile(__dirname+'/index.html');
 });
-app.get('/write',function(요청,응답){
-    응답.sendFile(__dirname+'/write.html');
-});
 
-app.post('/add',function(요청,응답){
-    응답.send('전송완료');
+
+
+app.get('/write',function(요청,응답){
+
+    db.collection('post').find().toArray(function(에러,결과){
+        console.log(결과);
+        응답.render('write.ejs', {posts:결과});
+    });
     
+});
+app.post('/add',function(요청,응답){
+    // 응답.send('전송완료');
+    db.collection('post').find().toArray(function(에러,결과){
+        console.log(결과);
+        응답.render('list.ejs', {posts:결과});
+    });
     db.collection('counter').findOne({name:'게시물갯수'},function(에러,결과){
         console.log(결과.totalPost);
         let 총게시물갯수=결과.totalPost;
